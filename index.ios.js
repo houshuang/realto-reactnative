7f0a9ab180cf1603115392deed6ddbe9ce2f29f5
Stian Håklev <shaklev@gmail.com>
2016-11-26 07:40:49 +0100

    Adding simple Meteor fetching

--- index.ios.js
+++ index.ios.js
@@ -12,24 +12,39 @@ import {
   View
 } from 'react-native';
 
-export default class realto2 extends Component {
+import Meteor, { createContainer, MeteorListView } from 'react-native-meteor';
+Meteor.connect('http://www.realto.ch/websocket')
+Meteor.loginWithPassword('stian.haklev@epfl.ch', 'Password', err => console.log(err))
+
+export class HomeScreen extends Component {
+    renderItem(e) {
+    return (e.title && e.title.length > 0 ? 
+      <View><Text>{e.title}</Text>
+      {e.text ? <Text>{e.text}</Text> : null}<Text>-------------</Text></View> : null)
+  }
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
           Welcome to React Native!
         </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+       <MeteorListView
+          collection="posts"
+          renderRow={this.renderItem}
+        />      
       </View>
     );
   }
 }
+
+export const realto2 = createContainer(params=>{
+  const handle = Meteor.subscribe('allPosts');
+ 
+  return {
+    todosReady: handle.ready(),
+  };
+}, HomeScreen)
+
 
 const styles = StyleSheet.create({
   container: {
